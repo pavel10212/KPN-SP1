ab22898fe7a8139b628b6fd773bbf633a3f7ba82
noephuket <157894276+NoeNeoK@users.noreply.github.com>
2024-07-10 15:48:55 +0700

    image

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -42,7 +42,9 @@ const LoginPage = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <Image
             className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            width={40}
+            height={40}
+            src="/@/app/img/logo.jpg"
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
