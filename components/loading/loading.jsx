34984c3f8280a14071543ff84b3316e88bb4dd91
noephuket <157894276+NoeNeoK@users.noreply.github.com>
2024-07-15 13:17:58 +0700

    loading fixed for mobile

--- components/loading/loading.jsx
+++ components/loading/loading.jsx
@@ -1,11 +1,15 @@
 const Loading = () => {
   return (
-    <div className="fixed inset-y-0 right-0 left-0 lg:left-64 z-50 flex items-center justify-center bg-gray-800 bg-opacity-90">
-      <div className="relative transform -translate-x-1/2 lg:translate-x-0 left-1/2 lg:left-0">
-        <div className="animate-spin rounded-full h-48 w-48 border-t-4 border-b-4 border-indigo-500"></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <div className="h-32 w-32 bg-white rounded-full flex items-center justify-center">
-            <div className="text-2xl font-bold text-indigo-500">Loading...</div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-90">
+      <div className="lg:ml-64 w-full lg:w-[calc(100%-16rem)] flex items-center justify-center">
+        <div className="relative">
+          <div className="animate-spin rounded-full h-48 w-48 border-t-4 border-b-4 border-indigo-500"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div className="h-32 w-32 bg-white rounded-full flex items-center justify-center">
+              <div className="text-2xl font-bold text-indigo-500">
+                Loading...
+              </div>
+            </div>
           </div>
         </div>
       </div>
