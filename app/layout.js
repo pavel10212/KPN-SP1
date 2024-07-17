885963d8d472c70c96029650b3fd5279a5b01ab5
unknown <u6430176@au.edu>
2024-07-17 14:36:01 +0700

    Bug fixes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { LoadingWrapper } from "@/components/loading/loadingWrapper";
 
 export const metadata = {
   title: "KPN Senior Project 1",
@@ -11,7 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          <LoadingWrapper>{children}</LoadingWrapper>
+        </SessionProvider>
         <SpeedInsights />
       </body>
     </html>
