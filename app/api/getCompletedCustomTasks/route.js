82c7b160eb1462f57f87e7360175a34da5e38de8
Pavel <u6430176@au.edu>
2024-10-16 21:14:18 +0700

    Readding

--- app/api/getCompletedCustomTasks/route.js
+++ app/api/getCompletedCustomTasks/route.js
@@ -1,15 +1,16 @@
 import prisma from "../prismaClient";
-import { NextResponse } from "next/server";
+import {NextResponse} from "next/server";
 
+
+//Test
 export async function GET() {
+    const CompletedCustomTasks = await prisma.customTask.findMany({
+        where: {
+            status: {
+                in: ["Completed", "Dropped Off"]
+            },
+        }
+    });
 
-  const CompletedCustomTasks = await prisma.customTask.findMany({
-    where: {
-      status: {
-        in: ["Completed", "Dropped Off"]
-      },
-    }
-  });
-
-  return NextResponse.json(CompletedCustomTasks);
+    return NextResponse.json(CompletedCustomTasks);
 }
